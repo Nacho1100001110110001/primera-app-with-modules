fccbd8cbc8f67cb8737823a7b59e80966c65d5da
Nacho <ignacio.rivas@sermaluc.cl>
2024-04-17 10:35:26 -0400

    some logic idk

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,26 @@ import { allIcons } from 'ngx-bootstrap-icons';
 })
 export class AppComponent {
   title = 'El pepe SA';
+  products = [{
+    imageSrc: "../../../assets/imgs/camara.jpg",
+    name: "Cámara fotográfica",
+    code: "tbx-0022",
+    date: new Date("02-27-2019"),
+    price: 12,
+    rating: 5
+  },{
+    imageSrc: "../../../assets/imgs/taladro.jpeg",
+    name: "Taladro de circuncisión",
+    code: "gmg-0042",
+    date: new Date("09-06-2016"),
+    price: 36,
+    rating: 4
+  },{
+    imageSrc: null,
+    name: "Notebook Toshiba",
+    code: "45sd54",
+    date: new Date("06-24-2020"),
+    price: 1000,
+    rating: 5
+  }];
 }
--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { allIcons } from 'ngx-bootstrap-icons';
 import { Producto } from '../../models/product';
 import { DatePipe } from '@angular/common';
@@ -8,33 +8,14 @@ import { DatePipe } from '@angular/common';
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.scss',
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit{
   showImage: boolean;
-  products: Producto[];
+  @Input() products !: Producto[];
   constructor() {
     this.showImage = true;
-    this.products = [{
-      imageSrc: "../../../assets/imgs/camara.jpg",
-      name: "Cámara fotográfica",
-      code: "tbx-0022",
-      date: new Date("02-27-2019"),
-      price: 12,
-      rating: 5
-    },{
-      imageSrc: "../../../assets/imgs/taladro.jpeg",
-      name: "Taladro de precusión",
-      code: "gmg-0042",
-      date: new Date("09-06-2016"),
-      price: 36,
-      rating: 4
-    },{
-      imageSrc: null,
-      name: "Notebook Toshiba",
-      code: "45sd54",
-      date: new Date("06-24-2020"),
-      price: 1000,
-      rating: 5
-    }]
+  }
+  ngOnInit(): void {
+    
   }
 
   imageToggle(){
